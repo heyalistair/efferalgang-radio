d2488454124990374e3a34bf58898d811f4efb09
Alistair Jones <alistair@ohalo.co>
2020-06-29 00:30:15 +0100

    Using production channel id

--- src/main/java/com/alistairj/frlgang/YouTubeService.java
+++ src/main/java/com/alistairj/frlgang/YouTubeService.java
@@ -29,8 +29,8 @@ public class YouTubeService {
 
   private static final Logger logger = LoggerFactory.getLogger(YouTubeService.class);
 
-  //public static final String EFFERALGANG_RADIO_CHANNEL_ID = "UCEhyiFmy5c6MrTY1iLz2bAQ";
-  public static final String EFFERALGANG_RADIO_CHANNEL_ID = "UC5Z2eMviso2vnK9iHnmJO8w";
+  public static final String EFFERALGANG_RADIO_CHANNEL_ID = "UCEhyiFmy5c6MrTY1iLz2bAQ";
+  //public static final String EFFERALGANG_RADIO_CHANNEL_ID = "UC5Z2eMviso2vnK9iHnmJO8w";
 
   private static final List<String> blacklistVideoIds = new ArrayList<>();
 
